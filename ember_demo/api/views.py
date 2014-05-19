723aa2c98e01ea25ecd28702b4fedddfd925e801
agconti <agc11d@gmail.com>
2014-05-19 19:01:13 -0400

    feat(api): created viewsets and wired them up

--- ember_demo/api/views.py
+++ ember_demo/api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from core.models import Product, Review
+from .serialiers import ProductSerializer, ReviewSerializer
 
-# Create your views here.
+class ProductViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides `list` and `detail` actions.
+    """
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+
+    Additionally we also provide an extra `highlight` action.
+    """
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
